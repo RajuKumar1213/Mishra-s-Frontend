3cfd375c9b8860ce3fe935ed176490af08b12f17
Raju kumar <rajuvis778@gmail.com>
2025-04-30 10:56:54 +0530

    some random solution.

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,26 +1,21 @@
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
+import GetStartedPage from "../pages/GetStartedPage";
 
 const Login = () => {
   const navigate = useNavigate();
 
   useEffect(() => {
     const role = localStorage.getItem("role");
+    console.log(role);
 
-    if (role == "Professional") {
-      navigate("/professional/dashboard");
-    } else if (role == "Customer") {
-      navigate("/customer-home");
-    } else if (role == "Company") {
-      navigate("/company/dashboard");
-    }
     // else stay on login page
   }, []);
 
   return (
     <div>
       {/* Login form goes here */}
-      <h2>Login Page</h2>
+      <GetStartedPage />
     </div>
   );
 };
